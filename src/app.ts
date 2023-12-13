3f41b0e8037f7c269c252876a12f1ea7ce6b50c7
Stina Collin <misumi_wear@yahoo.se>
2023-12-13 16:04:30 +0100

    Second test, POST status-201, 400 och 404

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,29 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to my API!');
 });
+
+app.post('/contact', (req, res) => {
+    const { firstname, lastname, email, personalnumber, address, zipCode, city, country } = req.body;
+  
+    if (!firstname) {
+      return res.status(400).json([{ error: 'firstname is missing' }]);
+    }
+  
+    if (!lastname) {
+      return res.status(400).json([{ error: 'lastname is missing' }]);
+    }
+  
+    res.status(201).json({});
+  });
+
+  // detta är en middleware som fångar upp alla requests som inte matchar någon av de routes som finns ovanför
+  app.use((req, res) => {
+    res.status(404).send('Not found');
+  }); 
 
 export default app;
--- test/app.test.ts
+++ test/app.test.ts
@@ -1,12 +1,91 @@
 import request from 'supertest';
 import app from '../src/app';
 
-// I mitt första test vill jag testa att jag får tillbaka statuskod 200 och ett meddelande från mitt API
+// I mitt första test GET, vill jag testa att jag får tillbaka statuskod 200 och ett meddelande, från mitt API
 describe('GET /', () => {
-  it('should return status code 200 and a welcome message', async () => {
+  it('should return status code 200 and a welcome message', async () => {  // Test för rotendpointen
     const response = await request(app).get('/');
 
     expect(response.status).toBe(200);
     expect(response.text).toBe('Welcome to my API!');
   });
+
+    it('should return status code 404 and an error message', async () => {  // Test för en endpoint som inte finns
+        const response = await request(app).get('/does-not-exist');
+    
+        expect(response.status).toBe(404);
+        expect(response.text).toBe('Not found');
+    });
+});
+
+// I mitt andra test POST vill jag testa att jag får tillbaka statuskod 201 vid giltig input, och statuskod 400 vid ogiltig input
+    describe('POST /contact', () => {
+        it('should return 201 with no content for valid input', async () => {
+          const response = await request(app)
+            .post('/contact')
+            .send({
+              "firstname": "Anna",
+              "lastname": "Andersson",
+              "email": "anna.andersson@gmail.com",
+              "personalnumber": "550713-1405",
+              "address": "Utvecklargatan 12",
+              "zipCode": "111 22",
+              "city": "Stockholm",
+              "country": "Sweden"
+            });
+      
+          expect(response.status).toBe(201);
+          expect(response.body).toEqual({}); 
+        });
+
+      // testar ghär även att jag får tillbaka statuskod 400 och ett felmeddelande, om jag skickar in en ogiltig input
+        it('should return 400 for invalid input', async () => {
+          const response = await request(app)
+            .post('/contact')
+            .send({
+              // Skickar här in en giltig input eftersom firstname saknas och förväntar mig att få tillbaka ett felmeddelande och statuskod 400
+              "lastname": "Andersson",
+              "email": "anna.andersson@gmail.com",
+              "personalnumber": "550713-1405",
+              "address": "Utvecklargatan 12",
+              "zipCode": "111 22",
+              "city": "Stockholm",
+              "country": "Sweden"
+            });
+      
+          expect(response.status).toBe(400);
+          expect(response.body).toEqual([
+            { "error": "firstname is missing" }            
+          ]);
+        });
+
+        it('should return 400 for invalid input', async () => {
+            const response = await request(app)
+              .post('/contact')
+              .send({
+                // Skickar här in en giltig input eftersom lastname saknas och förväntar mig att få tillbaka ett felmeddelande och statuskod 400
+                "firstname": "Anna",
+                "email": "anna.andersson@gmail.com",
+                "personalnumber": "550713-1405",
+                "address": "Utvecklargatan 12",
+                "zipCode": "111 22",
+                "city": "Stockholm",
+                "country": "Sweden"
+              });
+        
+            expect(response.status).toBe(400);
+            expect(response.body).toEqual([
+              { "error": "lastname is missing" }            
+            ]);
+          });
+
+          // lägger även in ett test för endpointen /does-not-exist, och förväntar mig att få tillbaka statuskod 404 och ett felmeddelande 
+          it('should return status code 404 and an error message', async () => {
+            const response = await request(app).get('/does-not-exist');
+            expect(response.status).toBe(404);
+            expect(response.text).toBe('Not found');
+
+        });
+        
+          
 });
